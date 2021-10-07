21e966a5a6e9dda8f5a31e1f6dc5f26a33ee06a9
nippy1113 <75803436+nippy1113@users.noreply.github.com>
2021-10-07 16:50:51 +0300

    changed driver

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,16 +1,16 @@
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
 pipeline {
-    agent {node { label 'linux'}}
+    agent any
     stages {
         stage('Build') {
             steps {
-                sh "${mvn} clean install"
+                sh 'mvn clean install'
             }
         }
         stage('Run Tests') {
             steps {
-                sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
+                sh "mvn install-Dcucumber.filter.tags =\"{TAG}\""
             }
         }
         stage('Allure Report Generation') {
